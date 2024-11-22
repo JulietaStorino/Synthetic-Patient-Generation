828b3688ed157e1b6ff0a2c1724fd5000d3231cb
JulietaStorino <julieta_storino@mi.unc.edu.ar>
2024-11-22 11:23:18 -0300

    modified high_risk_proffessions

--- data/health_record.py
+++ data/health_record.py
@@ -13,13 +13,13 @@ risk_diseases = [
 
 # List of some professions that are considered high risk
 high_risk_professions = [
-    "Médico", "Enfermera", "Técnico de Cuidados Auxiliares de Enfermería", "Paramédico", "Cirujano", "Dentista", "Farmacéutico",
+    "Médico", "Enfermera", "Técnico de Cuidados Auxiliares de Enfermería", "Paramédico", "Cirujano", "Soldador",
     "Trabajador de la Construcción", "Albañil", "Carpintero", "Electricista", "Plomero", "Pintor de Construcción",
-    "Trabajador Agrícola", "Granjero", "Jornalero", "Operador de Maquinaria Agrícola", "Trabajador de Riego",
-    "Trabajador Industrial", "Operador de Máquina", "Fabricante", "Trabajador de Línea de Producción", "Montador", "Soldador",
-    "Trabajador de Laboratorio", "Químico", "Técnico de Laboratorio", "Científico de Investigación", "Microbiólogo", "Técnico de Radiología",
+    "Trabajador Agrícola", "Granjero", "Operador de Maquinaria Agrícola", "Ama de casa", "Dentista",
+    "Trabajador Industrial", "Operador de Máquina", "Fabricante", "Trabajador de Línea de Producción", "Montador",
+    "Trabajador de Laboratorio", "Químico", "Técnico de Laboratorio", "Científico de Investigación", "Microbiólogo",
     "Trabajador de Servicios de Limpieza", "Conserje", "Limpiador", "Trabajador de Sanitización", "Trabajador de Mantenimiento",
-    "Trabajador de Logística y Transporte", "Conductor de Camión", "Operador de Maquinaria Pesada", "Repartidor", "Trabajador de Almacén",
+    "Conductor de Camión", "Operador de Maquinaria Pesada", "Repartidor", "Trabajador de Almacén", "Farmacéutico",
     "Trabajador de Minería", "Minero", "Operador de Equipos Mineros", "Ingeniero de Minas", "Inspector de Seguridad Minera",
-    "Bombero", "Policía", "Guardia de Seguridad", "Soldado", "Trabajador de Residuos Sólidos", "Operador de Plantas de Tratamiento"
+    "Bombero", "Policía", "Guardia de Seguridad", "Soldado", "Trabajador de Residuos Sólidos", "Técnico de Radiología"
 ]
