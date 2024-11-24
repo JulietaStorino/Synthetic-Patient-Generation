5400b233404ae01b5ed4b9f9ebaf68e73bff6b85
JulietaStorino <julieta_storino@mi.unc.edu.ar>
2024-11-24 01:05:02 -0300

    Report with possible relatives

--- data/report.py
+++ data/report.py
@@ -4,7 +4,13 @@ MESH_population_groups = [
     "de ascendencia asiática del sudeste", "de ascendencia asiática occidental", "de ascendencia caribeña", "de ascendencia indígena americana",
     "de ascendencia europea oriental", "de ascendencia nórdica o escandinava", "de ascendencia del medio oriente", "de ascendencia norteamericana",
     "de ascendencia oceánica", "de ascendencia australiana", "de ascendencia sudamericana", "de descendencia africana", "de ascendencia árabe",
-    "de ascendencia indígena centroamericana", "de ascendencia indígena sudamericana", "de ascendencia gitana", "de minorías étnicas", "de comunidades vulnerables",
-    "testigo de Jehová", "musulman", "judío", "refugiado", "migrante", "transeúnte", "vegetariano", "vegano", "víctima de desastre", "víctima de violencia de género",
-    "discapacitado", "terminal", "sobreviviente de violencia", "fumador", "exfumador", "no fumador", "consumidor de alcohol", "usuario de drogas",
+    "de ascendencia indígena centroamericana", "de ascendencia indígena sudamericana", "de ascendencia gitana", "de comunidades vulnerables", "testigo de Jehová",
+    "musulman", "judío", "refugiado", "migrante", "transeúnte", "vegetariano", "vegano", "víctima de desastre", "víctima de violencia de género", "discapacitado",
+    "terminal", "sobreviviente de violencia", "fumador", "exfumador", "no fumador", "consumidor de alcohol", "usuario de drogas"
+]
+
+# List of some possible relatives of a patient
+patient_companions = [
+    "abuela", "abuelo", "madre", "padre", "hermana", "hermano", "hija", "hijo", "tía", "tío", "prima", "primo", "sobrina", "sobrino", "nuera", "yerno", "suegra", "suegro",
+    "marido", "esposa", "pareja", "conviviente", "compañero", "amigo", "amiga", "vecino", "vecina", "compañero de trabajo", "compañera de trabajo", "compañero de estudio"
 ]
--- generator/report.py
+++ generator/report.py
@@ -1,27 +1,34 @@
 from random import choice
-from data.report import MESH_population_groups
+from data.report import MESH_population_groups, patient_companions
 from utils.utils import boolean_with_probability
 
 def generate_MESH_population_group():
     '''
-    Generates a random MESH population group from a list of common characteristics and if the person has it
+    Generates a random MESH population group from a list of common characteristics. It may return None with a 40% probability
     '''
-    have_mesh_population_group = boolean_with_probability(.6)
-    if have_mesh_population_group:
-        return choice(MESH_population_groups)
-    return None
+    return choice(MESH_population_groups) if boolean_with_probability(.6) else None
+
+def generate_patient_companion():
+    '''
+    Generates a random relative of a patient from a list of common relatives. It may return None with a 40% probability
+    '''
+    return choice(patient_companions) if boolean_with_probability(.6) else None
 
 def generate_report():
     """
-    Generates a report that includes a MESH population group (if any)
+    Generates a report that may include a MESH population group and a relative of a patient
     """
-    report = 'Paciente '
+    report = 'Paciente'
 
     mesh_group = generate_MESH_population_group()
+    companion = generate_patient_companion()
 
     if mesh_group:
-        report += f"{mesh_group}."
-    else:
-        report += "sin grupo poblacional MESH."
-
+        report += f" {mesh_group}"
+    
+    if companion:
+        report += f", acompañado de su {companion},"
+    
+    report += " se presenta a la consulta con los siguientes síntomas..."
+    
     return report
