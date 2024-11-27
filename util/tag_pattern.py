ad26eb076d536d8bc9731f6b0c09f87dd527fc90
JulietaStorino <julieta_storino@mi.unc.edu.ar>
2024-11-27 20:23:51 -0300

    Some refactorings

--- util/tag_pattern.py
+++ util/tag_pattern.py
@@ -28,25 +28,35 @@ tag_patterns = [
     (OTROS_SUJETO_ASISTENCIA, r"Paciente\s*(?:de\s*)?(.*?)\s*de\s*(\d+ años|\d+ meses|un año|un mes)?\s*de edad\s*(, acompañado de su\s*([^,]+),?)?\s*se presenta a la consulta con los siguientes síntomas\.\.\.")
 ]
 
+# Define the tag elements
+id_tag = [ID_SUJETO_ASISTENCIA, ID_TITULACION_PERSONAL_SANITARIO, ID_ASEGURAMIENTO, ID_CONTACTO_ASISTENCIAL, IDENTIF_VEHICULOS_NRSERIE_PLACAS, ID_EMPLEO_PERSONAL_SANITARIO]
+name_tag = [NOMBRE_SUJETO_ASISTENCIA, NOMBRE_PERSONAL_SANITARIO]
+location_tag = [CALLE, TERRITORIO, PAIS, INSTITUCION, HOSPITAL, CENTRO_DE_SALUD]
+contact_tag = [CORREO_ELECTRONICO, NUMERO_TELEFONO, NUMERO_FAX]
+other_tag = [SEXO_SUJETO_ASISTENCIA, FAMILIARES_SUJETO_ASISTENCIA, OTROS_SUJETO_ASISTENCIA]
+age_tag = [EDAD_SUJETO_ASISTENCIA]
+date_tag = [FECHAS]
+profession_tag = [PROFESION]
+
 def match_tag(tag_type, tags):
     """
     Returns the correct tag element based on the tag type.
     """
-    if tag_type in [NOMBRE_SUJETO_ASISTENCIA, NOMBRE_PERSONAL_SANITARIO]:
+    if tag_type in name_tag:
         return ET.SubElement(tags, NAME)
-    elif tag_type in [ID_SUJETO_ASISTENCIA, ID_TITULACION_PERSONAL_SANITARIO, ID_ASEGURAMIENTO, ID_CONTACTO_ASISTENCIAL, IDENTIF_VEHICULOS_NRSERIE_PLACAS, ID_EMPLEO_PERSONAL_SANITARIO]:
+    elif tag_type in id_tag:
         return ET.SubElement(tags, ID)
-    elif tag_type in [CALLE, TERRITORIO, PAIS, INSTITUCION, HOSPITAL, CENTRO_DE_SALUD]:
+    elif tag_type in location_tag:
         return ET.SubElement(tags, LOCATION)
-    elif tag_type == FECHAS:
+    elif tag_type in date_tag:
         return ET.SubElement(tags, DATE)
-    elif tag_type in [SEXO_SUJETO_ASISTENCIA, FAMILIARES_SUJETO_ASISTENCIA, OTROS_SUJETO_ASISTENCIA]:
+    elif tag_type in other_tag:
         return ET.SubElement(tags, OTHER)
-    elif tag_type == EDAD_SUJETO_ASISTENCIA:
+    elif tag_type in age_tag:
         return ET.SubElement(tags, AGE)
-    elif tag_type in [CORREO_ELECTRONICO, NUMERO_TELEFONO, NUMERO_FAX]:
+    elif tag_type in contact_tag:
         return ET.SubElement(tags, CONTACT)
-    elif tag_type == PROFESION:
+    elif tag_type in profession_tag:
         return ET.SubElement(tags, PROFESSION)
     
     return ET.SubElement(tags, TAG)
--- util/txt_to_xml.py
+++ util/txt_to_xml.py
@@ -12,27 +12,35 @@ def create_tag(tag_type, value, start, end, tags, tag_id):
     tag.set("comment", "")
     return tag_id + 1
 
-def process_name_subject_assistance_tag(tag_type, full_name, start_pos, tags, tag_id):
+def split_full_name(full_name):
     """
-    Split the full name into name and surnames, and create the corresponding tags.
+    Split the full name into name and surnames.
     """
     parts = full_name.split()
-    name = ""
-    surnames = ""
-
-    # If the person has two names and two surnames
     if len(parts) == 4:
         name = " ".join(parts[:2])
         surnames = " ".join(parts[2:])
     else:
         name = parts[0]
         surnames = " ".join(parts[1:])
+    return name, surnames
 
-    # Calculates the start and end positions for the name and surnames
+def calculate_positions(name, surnames, start_pos):
+    """
+    Calculate the positions for the name and surnames.
+    """
     first_start = start_pos
     first_end = first_start + len(name)
     last_start = first_end + 1 
     last_end = last_start + len(surnames)
+    return first_start, first_end, last_start, last_end
+
+def process_name_subject_assistance_tag(tag_type, full_name, start_pos, tags, tag_id):
+    """
+    Split the full name into name and surnames, and create the corresponding tags.
+    """
+    name, surnames = split_full_name(full_name)
+    first_start, first_end, last_start, last_end = calculate_positions(name, surnames, start_pos)
 
     # Create the tag for the name(s) and surnames
     tag_id = create_tag(tag_type, name, first_start, first_end, tags, tag_id)
